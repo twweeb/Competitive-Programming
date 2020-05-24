4d595960099d3b3be1681cb122dfada48327fdd0
Lei Hsiung <lei@leihsiung.me>
2020-05-24 22:06:07 +0800

    Solved A, Using Divide and Conquer

--- NCTU CPI/Midterm/A.cpp
+++ NCTU CPI/Midterm/A.cpp
@@ -1,8 +1,9 @@
 //
-//  A.cpp  (This would cause TLE.)
+//  A.cpp
+//  Concept: Divide and Conquer
 //  NCTU CPI Midterm
 //
-//  Created by Lei Hsiung on 2020/05/15.
+//  Created by Lei Hsiung on 2020/05/24.
 //  Copyright © 2020 Lei Hsiung. All rights reserved.
 //
 
@@ -10,27 +11,55 @@
 
 using namespace std;
 
-void solve() {
-    int n = 0 , k = 0;
-    cin >> n >> k;
-    vector <int> a(n), b(n);
-    int cost = 0;
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    for (int i = 0; i < n; ++i) cin >> b[i];
-    for (int i = 0; i < n; ++i)
+int k;
+map<int, int> goal;
+
+long long cost_dnc(vector<int> &vec)
+{
+    size_t n = vec.size();
+    if (n <= 1) return 0LL;
+    vector<int> left, right;
+    for (int i = 0; i < n/2; ++i) left.push_back(vec[i]);
+    for (int i = n/2; i < n; ++i) right.push_back(vec[i]);
+    
+    // Divide and Conquer
+    long long cost = cost_dnc(left) + cost_dnc(right);
+    vector<int> cnt(k);
+    for (auto c: left) cnt[c%k]++;
+    int p, q, r;
+    for (p = q = r = 0; p < left.size() && q < right.size(); )
     {
-        int tmp = b[i];
-        for (int j = 0; j < a.size(); ++j)
+        if (goal[left[p]] < goal[right[q]])
         {
-            if (a[j] == b[i]) 
+            --cnt[left[p]%k];
+            vec[r++] = left[p++];
+        }
+        else
+        {
+            for (int i = 0; i < k; ++i)
             {
-                a.erase (a.begin()+j);
-                break;
+                cost += cnt[i] * ((i+right[q])%k);
             }
-            cost += ((tmp%k) + (a[j]%k))%k ;
+            vec[r++] = right[q++];
         }
     }
-    cout << cost << '\n';
+    while (p < left.size()) vec[r++] = left[p++];
+    while (q < right.size()) vec[r++] = right[q++];
+    return cost;
+}
+
+void solve() {
+    int n = 0;
+    cin >> n >> k;
+    vector<int> orig(n);
+    for (int i = 0; i < n; ++i) cin >> orig[i];
+    for (int i = 0; i < n; ++i)
+    {
+        int t = 0;
+        cin >> t;
+        goal[t] = i;
+    }
+    cout << cost_dnc(orig) << '\n';
 }
 
 int main() {

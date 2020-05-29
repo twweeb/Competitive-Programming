e3b295a6aab987abae9b7236bb03700f012206b9
Lei Hsiung <lei@leihsiung.me>
2020-05-30 04:45:22 +0800

    Cantor Set

--- Programming Contest/Waterloo/2009/Cantor.cpp
+++ Programming Contest/Waterloo/2009/Cantor.cpp
@@ -1,8 +1,9 @@
 //
-//  C.cpp
-//  NCTU CPI Quiz 3
+//  Cantor.cpp
+//  Waterloo Programming Contest 2009-10-03
+//  Concept: Ternary expansion
 //
-//  Created by Lei Hsiung on 2020/05/01.
+//  Created by Lei Hsiung on 2020/05/13.
 //  Copyright © 2020 Lei Hsiung. All rights reserved.
 //
 
@@ -11,6 +12,33 @@
 using namespace std;
 
 void solve() {
+    string c = "";
+    while (getline(cin, c))
+    {
+        if (c[0] - 'E' == 0) break;
+        stringstream ss(c);
+        double t;
+        ss >> t;
+        bool isCantor = true;
+        for (int i = 0; i < 20; ++i) 
+        // Since having at most 6 digits after the decimal point, need around 20 times iteration to check.
+        {
+            if (t == 1 || t == 0) break;
+            if (t < 1) t*=3.0;
+            else if (t < 2)
+            {
+                isCantor = false;
+                break;
+            }
+            else if (t < 3) 
+            {
+                t -= 2.0;
+                t*=3.0;
+            }
+        }
+        if (isCantor) cout << "MEMBER\n";
+        else cout << "NON-MEMBER\n";
+    }
 }
 
 int main() {

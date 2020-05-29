4860ea8457b79e2e0bad16d2e5c5fe8129e57c83
Lei Hsiung <lei@leihsiung.me>
2020-05-30 02:14:04 +0800

    TravelingMonk

--- Programming Contest/Virginia Tech High School/2017/TravelingMonk.cpp
+++ Programming Contest/Virginia Tech High School/2017/TravelingMonk.cpp
@@ -1,8 +1,9 @@
 //
-//  G.cpp
-//  NCTU CPI Quiz 3
+//  TravelingMonk.cpp
+//  2017 Virginia Tech High School Programming Contest
+//  Concept: Binary Search
 //
-//  Created by Lei Hsiung on 2020/05/01.
+//  Created by Lei Hsiung on 2020/05/13.
 //  Copyright © 2020 Lei Hsiung. All rights reserved.
 //
 
@@ -11,12 +12,12 @@
 using namespace std;
 
 vector<pair<double,double> > _up, _down;
-double inf = 1 << 29;
+double inf = 1 << 29, mh;
 
-double heightAtTimeUP(double t)
+double upHeight(double t)
 {
     double q_h = 0.0, q_t = 0.0, p_h = 0.0, p_t = 0.0;
-    for(pair<double,double>p : _up)
+    for(pair<double, double>p : _up)
     {
         if(p.first >= t)
         {
@@ -29,10 +30,10 @@ double heightAtTimeUP(double t)
     return q_h + (p_h-q_h)*(t - q_t)/(p_t-q_t);
 }
 
-double heightAtTimeDOWN(double t)
+double downHeight(double t)
 {
     double q_h = 0.0, q_t = 0.0, p_h = 0.0, p_t = 0.0;
-    for(pair<double,double>p : _down)
+    for(pair<double, double>p : _down)
     {
         if(p.first >= t)
         {
@@ -45,27 +46,26 @@ double heightAtTimeDOWN(double t)
     return q_h + (p_h-q_h)*(t - q_t)/(p_t-q_t);
 }
 
-double err(double x)
+bool feasible(double x)
 {
-    return heightAtTimeUP(x)-heightAtTimeDOWN(x);
+    return upHeight(x) + downHeight(x) >= mh;
 }
 
-double bruteS(double left, double right)
+double binSearch(double left, double right)
 {
-    double a = (left + right)/2;
-    double tmp = err(a);
-
-    if (abs(tmp) < 0.000001) return a;
-    else if (tmp < 0) return bruteS(a, right);
-    else if (tmp > 0) return bruteS(left, a);
-    else return a;
+    while (right - left > 1e-7)
+    {
+        if (feasible((left+right)/2)) right = (left+right)/2;
+        else left = (left+right)/2;
+    }
+    return right;
 }
 
 void solve() {
     int up, down;
     double min_time = inf, h, t, prev_h, prev_t;
     cin >> up >> down;
-    _up.push_back(make_pair(0.0,0.0));
+    _up.push_back(make_pair(0.0,0.0)); _down.push_back(make_pair(0.0,0.0));
     prev_h = prev_t = 0;
     for (int i = 0; i < up; ++i)
     {
@@ -75,19 +75,17 @@ void solve() {
         _up.push_back(make_pair(t, h));
         prev_h = h; prev_t = t;
     }
-    _down.push_back(make_pair(0.0,prev_h));
-    min_time = prev_t; prev_t = 0;
+    mh = prev_h; min_time = prev_t; prev_t = prev_h = 0;
     for (int i = 0; i < down; ++i)
     {
         cin >> h >> t;
-        h = prev_h - h;
+        h += prev_h;
         t += prev_t;
         _down.push_back(make_pair(t, h));
         prev_h = h; prev_t = t;
     }
     min_time = (min_time < prev_t) ? min_time : prev_t;
-
-    cout << fixed << setprecision(6) << bruteS(0, min_time) <<endl;
+    cout << fixed << setprecision(6) << binSearch(-1, min_time+1) << '\n';
 }
 
 int main() {

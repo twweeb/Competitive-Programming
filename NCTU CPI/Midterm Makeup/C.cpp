4e4a123ac08f1a4a4e4a7ae43db3a793c52594f0
Lei Hsiung <lei@leihsiung.me>
2020-06-06 18:43:41 +0800

    If no Mod Inverse

--- NCTU CPI/Midterm Makeup/C.cpp
+++ NCTU CPI/Midterm Makeup/C.cpp
@@ -2,7 +2,7 @@
 //  C.cpp
 //  NCTU CPI Midterm Makeup
 //
-//  Created by Lei Hsiung on 2020/05/29.
+//  Created by Lei Hsiung on 2020/06/06.
 //  Copyright © 2020 Lei Hsiung. All rights reserved.
 //
 
@@ -10,38 +10,39 @@
 
 using namespace std;
 
-long long modPow(long long a, long long p, long long mod)
-{
-    if (p == 0) return 1;
-    if (p == 1) return a%mod;
-    if (p % 2 == 0)
+long long mod = 0LL;
+
+struct mat{
+    long long a, b, c, d, e, f, g, h, i;
+    mat(long long a, long long b, long long c,
+        long long d, long long e, long long f,
+        long long g, long long h, long long i) : a{a}, b{b}, c{c}, d{d}, e{e}, f{f}, g{g}, h{h}, i{i}{}
+
+    mat operator*(const mat& mul) const
     {
-        long long t = modPow(a, p/2, mod);
-        return (t*t)%mod;
+        return mat((a*mul.a+b*mul.d+c*mul.g) % mod, (a*mul.b+b*mul.e+c*mul.h) % mod, (a*mul.c+b*mul.f+c*mul.i) % mod,
+                   (d*mul.a+e*mul.d+f*mul.g) % mod, (d*mul.b+e*mul.e+f*mul.h) % mod, (d*mul.c+e*mul.f+f*mul.i) % mod,
+                   (g*mul.a+h*mul.d+i*mul.g) % mod, (g*mul.b+h*mul.e+i*mul.h) % mod, (g*mul.c+h*mul.f+i*mul.i) % mod);
     }
-    else 
-    {
-        long long t = modPow(a, (p-1)/2, mod);
-        return ((a%mod) * t * t)%mod;
+
+    mat pow(long long n){
+        if (n == 0) return mat(1LL, 0LL, 0LL, 0LL, 1LL, 0LL, 0LL, 0LL, 1LL);
+        if (n == 1) return *this;
+        mat half = (*this).pow(n/2);
+        if (n&1) return (*this)*half*half;
+        else return half*half;
     }
-}
+};
 
 void solve() {
-    int n = 0;
-    cin >> n;
-    while (n--)
+    int T = 0;
+    cin >> T;
+    while (T--)
     {
-        long long b, n, m;
-        cin >> b >> n >> m;
-        long long ans = 0LL;
-        b %= m;
-        for (int i = 1; i <= n; ++i)
-        {
-            cout << i << endl;
-            ans += ((i%m) * modPow(b,i,m));
-            ans %= m;
-        }
-        cout << ans << '\n';
+        long long b, n;
+        cin >> b >> n >> mod;
+        mat f = mat(b, 0LL, 0LL, b, b, 0LL, 0LL, 1LL, 1LL).pow(n);
+        cout << (b*(f.g+f.h))%mod << '\n';
     }
 }
 

83d5399b2d92d29bb7705f2aee8784d30dcbc194
Lei Hsiung <nthu.email@gmail.com>
2021-04-12 21:25:39 +0800

    TLE Fixed.

--- Programming Contest/CodeJam/2021/Round 1A/Prime_Time.cpp
+++ Programming Contest/CodeJam/2021/Round 1A/Prime_Time.cpp
@@ -11,100 +11,53 @@
 using namespace std;
 
 #define INF 1<<30
+#define MAX_P 95
+#define MAX_MUL_SUM 3026
 
-vector<long long> primes_list;
+long long primes_list[MAX_P], primes_num[MAX_P];
+int M;
 
-long long count_score(map<long long, int> &group)
+bool prime_decomp(long long score, long long mul_sum)
 {
-    long long score = 1;
-    for (auto val=group.begin(); val!=group.end(); ++val)
-        score *= pow(val->first, val->second);
-    return score;
-}
-
-void print_res(map<long long, int>& decomp, int init_sum)
-{
-    cout << "mul_sum: " << init_sum << ", mul decomposition: ";
-    if (!decomp.empty())
-    {
-        for (auto val=decomp.begin(); val!=decomp.end(); ++val)
-            for (int i = 0; i < val->second; ++i) cout << val->first << " ";
-    }
-    else cout << "Unable to decompose.";
-    cout << '\n';
-}
-
-void prime_decomp(map<long long, int>& decomp_res, long long add_sum, long long mul_sum)
-{
-    long long cur_prime = 0ll, decomp_mul = 0ll;
-    for (int i = 0; i < primes_list.size(); ++i) 
+    long long cur_prime = 0ll, decomp_mul = 0ll, cnt = 0ll;
+    bool early_break = false;
+    for (int i = 0; i < M; ++i) 
     {
         cur_prime = primes_list[i];
-        while (add_sum != 1 && add_sum%cur_prime == 0)
+        cnt = 0ll;
+        while (score%cur_prime == 0)
         {
-            if (decomp_res.find(cur_prime)!=decomp_res.end()) ++ decomp_res[cur_prime];
-            else decomp_res[cur_prime] = 1;
-            add_sum /= cur_prime;
-            decomp_mul += cur_prime;
+            ++cnt;
+            if (cnt > primes_num[i]) return false;
+            else score /= cur_prime;
         }
-        if (add_sum == 1) break;
-    }
-    if (add_sum != 1 || mul_sum != decomp_mul) decomp_res.clear();
-    // print_res((*decomp_res), mul_sum);
-}
 
-bool is_feasible(map<long long, int>& primes, map<long long, int>& decomp_of_mul_sum)
-{
-    for (auto prime=decomp_of_mul_sum.begin(); prime!=decomp_of_mul_sum.end(); ++prime)
-    {
-        if (primes.find(prime->first) != primes.end())
-        {
-            if (primes[prime->first] >= prime->second) continue;
-            else return false; 
-        }
-        else return false;
+        decomp_mul += cur_prime*cnt;
+        if (score == 1ll) break;
     }
-    return true;
+    if (score != 1ll || mul_sum != decomp_mul) return false;
+    else return true;
 }
 
 void case_solve()
 {
-    int M;
     cin >> M;
-    map<long long, int> primes;
-    primes_list.clear();
-    long long prime, sum = 0ll;
-    int num;
+    long long sum = 0ll;
+    memset(primes_list, 0ll, M); memset(primes_num, 0ll, M);
+
     for (int i = 0; i < M; ++i)
     {
-        cin >> prime >> num;
-        primes[prime] = num;
-        primes_list.push_back(prime);
-        sum += prime*num;
+        cin >> primes_list[i] >> primes_num[i];
+        sum += primes_list[i]*primes_num[i];
     }
 
-    long long score = 0, MAX_SCORE = 0;
-    bool feasible = false;
-    // cout << '\n';
-    map<long long, int> decomp_of_mul_sum;
-    for (long long mul_sum = 2ll; mul_sum < sum; ++mul_sum)
+    long long score = 0ll;
+    for (long long mul_sum = 2ll; mul_sum < ((sum < MAX_MUL_SUM) ? sum : MAX_MUL_SUM); ++mul_sum)
     {
-        prime_decomp(decomp_of_mul_sum, sum-mul_sum, mul_sum);
-        if (!decomp_of_mul_sum.empty())
-        {
-            feasible = is_feasible(primes, decomp_of_mul_sum);
-            if (feasible)
-            {
-                score = count_score(decomp_of_mul_sum);
-                // cout << "score: " << score << '\n';
-                if (score > MAX_SCORE)  MAX_SCORE = score;
-            }
-            decomp_of_mul_sum.clear();
-        }
-        else continue;
-        feasible = false;
+        if (prime_decomp(score = sum-mul_sum, mul_sum)) break;
+        else score = 0ll;
     }
-    cout << MAX_SCORE << '\n';
+    cout << score << '\n';
 }
 
 void solve() {
